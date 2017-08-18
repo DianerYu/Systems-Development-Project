fb7a16a314327973da7fd26632c134c57af5bd04
DianerYu <30624719+DianerYu@users.noreply.github.com>
2017-08-18 21:50:15 +1000

    added android app

---
+++ app/src/main/java/developer/alfred/testabc/Test123.java
@@ -0,0 +1,13 @@
+package developer.alfred.testabc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Test123 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test123);
+    }
+}
